42237aa31a081787b44218ddf4c2d04b29bf73b9
oliviermeslin <olivier.meslin@insee.fr>
2025-10-18 20:45:19 +0000

    WIP

--- test.py
+++ test.py
@@ -1,27 +1,22 @@
 # %%
 import os
+import re
 import requests
 from osgeo import gdal
-import boto3
+import s3fs
+import py7zr
 
 # Function to download a file from a URL
 def download_file(url, local_path):
     response = requests.get(url)
-    response.raise_for_status()
-    with open(local_path, 'wb') as f:
+    with open(local_path+url.rsplit('/', 1)[-1], 'wb') as f:
         f.write(response.content)
-    print(f"Downloaded {url} to {local_path}")
+    print(f"Downloaded {url} to {local_path+url.rsplit('/', 1)[-1]}")
 
 # Function to convert ASC to GeoTIFF using GDAL
 def convert_asc_to_tiff(asc_path, tiff_path):
     gdal.Translate(tiff_path, asc_path, format='GTiff')
     print(f"Converted {asc_path} to {tiff_path}")
-
-# Function to upload file to S3
-def upload_to_s3(local_path, bucket_name, s3_key):
-    s3_client = boto3.client('s3')
-    s3_client.upload_file(local_path, bucket_name, s3_key)
-    print(f"Uploaded {local_path} to s3://{bucket_name}/{s3_key}")
 
 # Main process
 def process_asc_files(asc_urls, local_dir, bucket_name):
@@ -43,12 +38,28 @@ def process_asc_files(asc_urls, local_dir, bucket_name):
         # Upload GeoTIFF to S3
         upload_to_s3(tiff_path, bucket_name, tiff_filename)
 # %%
+os.mkdir("./rawdata")
+os.mkdir("./data")
 
+
+py7zr
+# %%
 # Example usage
 asc_urls = [
-    'https://example.com/elevation1.asc',
-    'https://example.com/elevation2.asc'
+    'https://data.geopf.fr/telechargement/download/BDALTI/BDALTIV2_2-0_25M_ASC_LAMB93-IGN69_D001_2023-08-08/BDALTIV2_2-0_25M_ASC_LAMB93-IGN69_D001_2023-08-08.7z'
 ]
+
+# %%
+archivename = asc_urls[0].rsplit('/', 1)[-1]
+download_file(asc_urls[0], "./rawdata/")
+# %%
+with py7zr.SevenZipFile("./rawdata/"+archivename, mode='r') as archive:
+    list_files = [f for f in archive.namelist() if re.search('1_DONNEES_LIVRAISON.*\\.asc', f)]
+    archive.extract(path='./data/', targets=list_files)
+
+
+
+# %%
 local_directory = 'downloaded_asc'
 s3_bucket = 'your-s3-bucket-name'
 
@@ -60,3 +71,5 @@ https://data.geopf.fr/telechargement/download/BDALTI/BDALTIV2_2-0_25M_ASC_LAMB93
 
 
 
+
+# %%
