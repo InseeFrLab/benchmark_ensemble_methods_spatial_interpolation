c9645758178837d99fd6970bc69c1b0ed537c7a5
oliviermeslin <olivier.meslin@insee.fr>
2025-10-22 20:28:43 +0000

    WIP

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import os
 from osgeo import gdal
 import s3fs
 import re
-# %%
+
 # Connect to S3
 fs = s3fs.S3FileSystem(anon=False)  # set anon=True for public buckets
 
@@ -15,8 +15,9 @@ path = 's3://oliviermeslin/BDALTI/'
 
 # List all files recursively
 files = fs.ls(path, detail=False)
-# %%
-for file in files[0:1]:
+
+# Loop over all files and export data as Parquet
+for file in files:
     print(file)
 
     # Extract the area
@@ -80,77 +81,6 @@ for file in files[0:1]:
             use_pyarrow=True,
             pyarrow_options={"partition_cols": ["departement"]}
         )
-
-# %%
-from osgeo import gdal, ogr
-
-# Open the raster
-src_ds = gdal.Open("BDALTI.tif")
-src_band = src_ds.GetRasterBand(1)
-
-# Create output shapefile
-driver = ogr.GetDriverByName("ESRI Shapefile")
-out_ds = driver.CreateDataSource("BDALTI.shp")
-out_layer = out_ds.CreateLayer("polygons", geom_type=ogr.wkbPolygon, srs=None)
-
-# Add an attribute field to the shapefile
-field_defn = ogr.FieldDefn("value", ogr.OFTReal)
-out_layer.CreateField(field_defn)
-
-# Perform polygonization
-gdal.Polygonize(src_band, None, out_layer, 0, [], callback=None)
-# %%
-os.system("mc cp BDALTI.shp s3/oliviermeslin/BDALTI2/BDALTI_D035.shp")
-os.system("mc cp BDALTI.dbf s3/oliviermeslin/BDALTI2/BDALTI_D035.dbf")
-os.system("mc cp BDALTI.shx s3/oliviermeslin/BDALTI2/BDALTI_D035.shx")
-
-# %%
-ds = gdal.Open("BDALTI.tif")
-band = ds.GetRasterBand(1)
-
-# Read raster data
-data = band.ReadAsArray()
-nodata = band.GetNoDataValue()
-
-# Create mask for valid data pixels
-if nodata is not None:
-    valid_mask = data != nodata
-else:
-    # If no nodata value assigned, create mask with non-zero data (optional)
-    valid_mask = np.ones(data.shape, dtype=bool)
-
-# Geotransform
-gt = ds.GetGeoTransform()
-rows, cols = data.shape
-col_indices = np.arange(cols)
-row_indices = np.arange(rows)
-col_grid, row_grid = np.meshgrid(col_indices, row_indices)
-
-# Calculate coordinates for all pixels
-x_coords = gt[0] + col_grid * gt[1] + row_grid * gt[2]
-y_coords = gt[3] + col_grid * gt[4] + row_grid * gt[5]
-
-# Flatten all arrays
-x_flat = x_coords.flatten()
-y_flat = y_coords.flatten()
-values_flat = data.flatten()
-mask_flat = valid_mask.flatten()
-
-# Assign NaN to invalid pixels
-values_flat = np.where(mask_flat, values_flat, np.nan)
-
-# %%
-# Convert to Polars
-df = pl.DataFrame({
-    'x': x_flat,
-    'y': y_flat,
-    'value': values_flat,
-    'departement': "A AJOUTER"
-})
-df.head(10)
-
-# Write to S3
-
 
 # %%
 import matplotlib.pyplot as plt
