409ec73daf7f5214db8bb6fc837238c5b3e78b47
oliviermeslin <olivier.meslin@insee.fr>
2025-10-22 20:12:44 +0000

    WIP

--- test.py
+++ test.py
@@ -4,8 +4,67 @@ import polars as pl
 import matplotlib.pyplot as plt
 import os
 from osgeo import gdal
+import s3fs
+import re
+# %%
+# Connect to S3
+fs = s3fs.S3FileSystem(anon=False)  # set anon=True for public buckets
 
-os.system(f"mc cp s3/oliviermeslin/BDALTI/BDALTI_D075.tif BDALTI.tif")
+# Define your S3 path
+path = 's3://oliviermeslin/BDALTI/'
+
+# List all files recursively
+files = fs.ls(path, detail=False)
+# %%
+for file in files:
+    print(file)
+
+    # Extract the area
+    area = re.search(r'(\w{2})\.tif$', file).group(1)
+    print(area)
+
+    # Download the data
+    os.system(f"mc cp s3/{file} BDALTI.tif")
+
+    # Open the TIFF file
+    dataset = gdal.Open('BDALTI.tif', gdal.GA_ReadOnly)
+    band = dataset.GetRasterBand(1)
+
+    # Get NODATA value
+    nodata = band.GetNoDataValue()
+
+    # Create mask for valid data pixels
+    if nodata is not None:
+    valid_mask = data != nodata
+    else:
+    # If no nodata value assigned, create mask with non-zero data (optional)
+    valid_mask = np.ones(data.shape, dtype=bool)
+
+    # Geotransform
+    gt = ds.GetGeoTransform()
+    rows, cols = data.shape
+    col_indices = np.arange(cols)
+    row_indices = np.arange(rows)
+    col_grid, row_grid = np.meshgrid(col_indices, row_indices)
+
+    # Calculate coordinates for all pixels
+    x_coords = gt[0] + col_grid * gt[1] + row_grid * gt[2]
+    y_coords = gt[3] + col_grid * gt[4] + row_grid * gt[5]
+
+    # Flatten all arrays
+    x_flat = x_coords.flatten()
+    y_flat = y_coords.flatten()
+    values_flat = data.flatten()
+    mask_flat = valid_mask.flatten()
+
+    # Assign NaN to invalid pixels
+    values_flat = np.where(mask_flat, values_flat, np.nan)
+
+
+
+
+
+# %%
 
 # %%
 # Open the TIFF file
